1a51858ccd603acd6497f88f71ae1e18909a1418
prajakta <prajakta.kamble@wohlig.com>
2018-01-08 21:27:22 +0530

    mandatory fields added and changes for modal after submitting the form

--- frontend/js/controllers/employment_opportunities.js
+++ frontend/js/controllers/employment_opportunities.js
@@ -137,19 +137,19 @@
          });
      };
      $scope.submitForm = function (employeeForm) {
-         NavigationService.saveEmployeeForm(employeeForm, function (data) {
-             console.log("!!!!!!!!!!!!submitForm(employeeForm)", data);
-             if (data.data) {
-                 //  NavigationService.sendEmployeeApplication(data.data.data, function (data) {
-                 //      console.log("send email to applicant", data.data);
-                 //  });
-                 $scope.openSubmitTextForm();
-                 $timeout(function () {
-                     //  $uibModalInstance.dismiss('cancel');
-                     $state.reload();
-                 }, 1000);
-             }
-         })
+         //  NavigationService.saveEmployeeForm(employeeForm, function (data) {
+         //      console.log("!!!!!!!!!!!!submitForm(employeeForm)", data);
+         //      if (data.data) {
+         //  NavigationService.sendEmployeeApplication(data.data.data, function (data) {
+         //      console.log("send email to applicant", data.data);
+         //  });
+         $scope.openSubmitTextForm();
+         $timeout(function () {
+             //  $uibModalInstance.dismiss('cancel');
+             $state.reload();
+         }, 1000);
+         //      }
+         //  })
      }
 
  });
